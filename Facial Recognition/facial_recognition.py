3490ab1058b06b727bd38443007c9061ce543c7a
Caio Moreira <caiovitor.moreira@hotmail.com>
2019-11-08 09:17:49 -0300

    Changing Facial Recognition Project

--- Facial Recognition/facial_recognition.py
+++ Facial Recognition/facial_recognition.py
@@ -1,132 +1,148 @@
 import os
 import numpy as np
 import cv2
+import pandas as pd
 from PIL import Image as img
 
 
 # "Global" variables, containing the paths of the most relevant directories
 current_file_path = os.path.dirname(__file__)
-image_path = os.path.join(current_file_path, "images")
-result_path = os.path.join(current_file_path,"faces")
-training_data = os.path.join(current_file_path,"training_data")
+images_path = os.path.join(current_file_path, "images")
+test_data_path = os.path.join(current_file_path,"test_data")
+train_data_path = os.path.join(current_file_path,"train_data")
 
-def save_faces(cascade_classifier, img_name):
+def get_sub_image(coordinates, image):
+    """ Function that cuts a given image using given coordinates.
+  
+    Keyword arguments: 
+        coordinates -- coordinates of where to cut the image 
+        image -- image to cut from. must be numpy array.
+
+    Returns:
+        sub_image -- cut from original image.
+    """
+
+    # Saving coordinates of the face inside the "root" image
+    x, y, w, h = coordinates
+
+    # Saving the numpy array of the face, using coordinates
+    sub_image = image[y:y + h, x:x + w]
+
+    return sub_image
+
+def get_faces(image):
+    """ Function that returns all the faces in a given image.
+
+    This function selects every face detected in a given image and returns them in a list.
+  
+    Keyword arguments: 
+        image -- image to select faces from. must be numpy array.
+    """
+    
+    faces = []
+
+    for index, face_coordinates in enumerate(cascade_classifier.detectMultiScale(image)):
+        faces.append(get_sub_image(face_coordinates, image))
+
+    return faces
+
+def save_faces(cascade_classifier, img_filename):
     """ Function that saves all the faces in a given image.
 
     This function selects every face detected in a given image and saves it.
   
     Keyword arguments: 
         cascade_classifier -- the object of the cascade classifier
-        img_name -- name of the image.
+        img_filename -- name of the image.
     """
     
     # Opening the image and converting it to grayscale, then converting it to a numpy array
-    image = img.open(os.path.join(image_path, img_name)).convert('L')
-    image_array = np.array(image, 'uint8')
+    image = img.open(os.path.join(images_path, img_filename)).convert('L')
+    image_as_array = np.array(image, 'uint8')
 
-    #Codigo que itera em cada rosto encontrado na foto aberta acima
-    for i, face in enumerate(cascade_classifier.detectMultiScale(image_array)):
-        #Salva as coordenadas da sub imagem encontrada
-        x, y, w, h = face
-        
-        #Salva a sub imagem encontrada na variavel abaixo usando as coordenadas
-        sub_face = image_array[y:y + h, x:x + w]
+    # Iterating through each face detected with detectMultiScale
+    # detectMultiScale returns the coordinates of the faces inside the image
+    for index, face in enumerate(get_faces(image_as_array)):
+        # Creates the path that will receive the output images if it doesn't exist
+        if not os.path.exists(test_data_path):
+            os.makedirs(test_data_path)
 
-        #Cria a pasta de resultados caso nao exista
-        if not os.path.exists(result_path):
-            os.makedirs(result_path)
-
-        #Salva a imagem na pasta criada acima
-        cv2.imwrite(os.path.join(result_path, "{}_0{}.jpg".format(img_name, i)), sub_face)
-
+        # Saves the face to the path
+        # Naming pattern: rootImgName_index.jpg
+        cv2.imwrite(os.path.join(
+            test_data_path,
+            '{}_{}.jpg'.format(img_filename.replace('.jpg', ''), index)),
+            face
+        )
 
 if __name__ == "__main__":
-    
-    #Inicializa o classificador em cascata atraves do path do arquivo
-    face_cascade = os.path.join(current_file_path, "haarcascade_frontalface_default.xml")
-    cascade_classifier = cv2.CascadeClassifier(face_cascade)
+    # Instantiates the cascadeClassifier from .xml file
+    cascade_path = os.path.join(current_file_path, "haarcascade_frontalface_default.xml")
+    cascade_classifier = cv2.CascadeClassifier(cascade_path)
 
-    #Itera pelas imagens na pasta image_path e segmenta cada uma delas
-    for f in [f for f in os.listdir(image_path) if os.path.isfile(os.path.join(image_path, f))]:
-        #Chama a funcao que segmenta e salva as sub faces
-        save_faces(cascade_classifier, f)
+    # Iterates through all the files in the images folder
+    # this will populate the test_data folder
+    for image_filename in os.listdir(images_path):
+        # Saves each face in each image
+        save_faces(cascade_classifier, image_filename)
 
-    #Cria o reconhecedor que usa o algoritmo LBPH
+    # Instantiates LBPH face recognizer
     face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-    
-    #Inicializa o dicionario da turma para que se possa dar a presença depois
-    turma = {}
 
-    #Inicializa os vetores que vao guardar as faces para treinamento e os nomes das faces
-    faces = []
+    # Lists to store the faces detected from train data and respective labels
+    train_faces = []
     labels = []
 
-    #Itera por cada pasta de sujeito na pasta de treinamento
-    for i, subject in enumerate(os.listdir(training_data)):
-        
-        #Adiciona o nome do sujeito como chave no dicionario da turma com o valor default "Faltou"
-        turma[str(subject)] = "Faltou"
+    # Iterates through each subject in train data
+    # each one must have its own folder containing train data
+    for subject in os.listdir(train_data_path):
+        # Saves the path to current subject's train data
+        subject_path = os.path.join(train_data_path, subject)
 
-        #Cria uma variavel que tem o path para o sujeito atual
-        local_path = os.path.join(training_data, subject)
+        # Goes through each photo of the subject in directory
+        for image_name in os.listdir(subject_path):
+            image = img.open(os.path.join(subject_path, image_name)).convert('L')
+            image_as_array = np.array(image, 'uint8')
 
-        #Itera em cada foto de treinamento do sujeito atual no path dele
-        for face in os.listdir(local_path):
+            # Using [0] because if there's only one face (the case here)
+            # the get_faces() method will still return a one item list 
+            face = get_faces(image_as_array)[0]
 
-            #Inicializa e converte a imagem como na funcao save_faces
-            face_file = img.open(os.path.join(local_path, face)).convert('L')
-            face_final = np.array(face_file, 'uint8')
-
-            #Segmenta a imagem, que so tem um rosto, o do sujeito, para armazena-la
-            face_points = cascade_classifier.detectMultiScale(face_final)
-            #Salva as coordenadas do rosto segmentado
-            x, y, w, h = face_points[0]
-            #Adiciona na lista de faces o rosto segmentado atraves de suas coordenadas
-            faces.append(face_final[y:y + h, x:x + w])
-            #Adiciona o nome do sujeito no array de nomes para dar nome a face encontrada acima
-            #Converte para int por causa das restricoes de tipo do metodo de treinamento abaixo
+            train_faces.append(face)
             labels.append(int(subject))
 
-    #Treina o reconhecedor com os arrays inicializados acima
-    face_recognizer.train(faces, np.array(labels))
+    # Trains the recognizer using train data and labels, labels must be a numpy array
+    face_recognizer.train(train_faces, np.array(labels))
 
-    #Passa por cada rosto no path de resultados, que contem os rostos segmentados da imagem da sala
-    for face_file in os.listdir(result_path):
-
-        #Carrega a imagem
-        face = cv2.imread(os.path.join(result_path, face_file))
+    # Goes through each face detected in test path
+    for face_filename in os.listdir(test_data_path):
+        # Loads images as cv2 image object and saves its dimensions
+        # 0 tells the imread method to open image in grayscale
+        face = cv2.imread(os.path.join(test_data_path, face_filename), 0)
+        height, width = face.shape
         
-        #Converte ela para grayscale(nivel de cinza)
-        gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        # Other way of converting the image to grayscale
+        # gray_scale = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-        #Prediz qual pessoa esta na imagem, poe o resultado no formato de tupla de duas posicoes
-        prediction = face_recognizer.predict(gray)
+        prediction = face_recognizer.predict(face)
 
-        #Salva o nome e a confianca, que estao na primeira e segunda posicao da tupla, respectivamente
-        name = str(prediction[0])
+        predicted_label = str(prediction[0])
         confidence = prediction[1]
 
-        #Printa os resultados para avaliacao
-        #print(name, confidence)
-        
-        #Da a presenca para o aluno predito
-        turma[name] = "Presente"
-        
-        #Cria uma janela nomeada com o nome predito pelo algoritmo
-        cv2.namedWindow(name, cv2.WINDOW_NORMAL)
-        #Posiciona essa janela na posicao (100, 100) do monitor
-        cv2.moveWindow(name, 100, 100)
-        #Mantem a janela com tamanho e proporcoes normais, evitando a tela cheia, que pode distorcer a imagem
-        cv2.setWindowProperty(name, cv2.WINDOW_NORMAL, cv2.WINDOW_KEEPRATIO)
-        #Mostra a janela nomeda no monitor, com a foto que foi usada para predizer
-        #Possibilita a avaliacao da precisao do algoritmo
-        cv2.imshow(name, face)
-        #Metodo que espera qualquer tecla ser pressionada para dar prosseguimento ao codigo
+        # Sets a name for the window that will be shown
+        # Using both predicted and true labels for evaluation
+        window_name = str('Predicted: ' + predicted_label + ' Actual: ' + face_filename)
+        # Creates a window
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        # Moves the window to [100, 100] in the screen
+        cv2.moveWindow(window_name, 100, 100)
+        # Mantains window in its original proportions, avoiding distortions
+        # Multiplying by 3 so that the name of the window is visible
+        cv2.resizeWindow(window_name, height*3, width*3)
+        # Shows the window with the face inside
+        cv2.imshow(window_name, face)
+        # Waits for any key to move on
         cv2.waitKey(0)
-        #Metodo que fecha todas as janelas abertas pelo metodo cv2.imshow()
+        # Kills the window, so that in the next iteration
+        # another window can be opened without crowding the screen
         cv2.destroyAllWindows()
-        
-    print()
-    #Printa o dicionario da turma para avaliacao dos resultados
-    for aluno in turma:
-        print(aluno, turma[aluno])
